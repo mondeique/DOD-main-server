ceb993a1a0437ea08f323505231de772f3c6cb50
Ubuntu <ubuntu@ip-172-31-3-126.ap-northeast-2.compute.internal>
2021-06-08 06:00:58 +0000

    [ADDED] S3 settings

--- dod/dod/settings.py
+++ dod/dod/settings.py
@@ -59,7 +59,8 @@ THIRD_APPS = [
     'ckeditor_uploader',
     'rest_framework',
     'pymysql',
-    # 'wpadmin'
+    # 'wpadmin',
+    'storages'
 ]
 
 INSTALLED_APPS += SECONDS_APPS + THIRD_APPS
@@ -141,8 +142,32 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+# AWS
+AWS_ACCESS_KEY_ID = SETTING_DEV_DIC['S3']['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = SETTING_DEV_DIC['S3']['AWS_SECRET_ACCESS_KEY']
+AWS_DEFAULT_ACL = SETTING_DEV_DIC['S3']['AWS_DEFAULT_ACL']
+AWS_S3_REGION_NAME = SETTING_DEV_DIC['S3']['AWS_S3_REGION_NAME']
+AWS_S3_SIGNATURE_VERSION = SETTING_DEV_DIC['S3']['AWS_S3_SIGNATURE_VERSION']
+AWS_STORAGE_BUCKET_NAME = SETTING_DEV_DIC['S3']['AWS_STORAGE_BUCKET_NAME']
 
-STATIC_URL = '/static/'
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_S3_REGION_NAME
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
+STATIC_LOCATION = 'statics'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_HOST, STATIC_LOCATION)
+STATICFILES_STORAGE = 'dod.storage.StaticStorage'
+
+MEDIA_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_HOST, MEDIA_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'dod.storage.CustomS3Boto3Storage'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_ROOT = "https://%s/statics/" % AWS_S3_CUSTOM_DOMAIN
+MEDIA_ROOT = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
