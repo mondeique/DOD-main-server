431a541ede5e30cfe6b33d0d1f453ac4f152f916
Ubuntu <ubuntu@ip-172-31-6-130.ap-northeast-2.compute.internal>
2021-06-30 00:43:58 +0000

    [UNDO] aws settings -> dod.settings.__init__

--- dod/dod/settings/aws.py
+++ dod/dod/settings/aws.py
@@ -3,6 +3,7 @@ import os
 ENV_SETTINGS_MODE = os.getenv('SETTINGS_MODE', '')
 
 if ENV_SETTINGS_MODE == 'prod':
+    print('0-----')
     from dod.settings.production import *
 elif ENV_SETTINGS_MODE == 'stag':
     from dod.settings.stagging import *
--- dod/dod/wsgi.py
+++ dod/dod/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dod.settings.aws')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dod.settings')
 
 application = get_wsgi_application()
