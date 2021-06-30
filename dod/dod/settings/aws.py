c6d865a3a02d778f1ec3fa48587c61c50693fa5b
JunyongPark97 <pjyong68@naver.com>
2021-06-30 00:32:18 +0000

    [UPDATED] Settings env divide by aws

---
+++ dod/dod/settings/aws.py
@@ -0,0 +1,10 @@
+import os
+
+ENV_SETTINGS_MODE = os.getenv('SETTINGS_MODE', '')
+
+if ENV_SETTINGS_MODE == 'prod':
+    from dod.settings.production import *
+elif ENV_SETTINGS_MODE == 'stag':
+    from dod.settings.stagging import *
+elif ENV_SETTINGS_MODE == 'devel':
+    from dod.settings.develop import *
